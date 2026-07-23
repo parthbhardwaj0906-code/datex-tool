bacbb117fc93f13c3e78cfb1444955d3d861b381
parthbhardwaj0906-code <parthbhardwaj0906@gmail.com>
2026-07-23 05:44:28 +0530

    Update import statement for Flask app

--- api/index.py
+++ api/index.py
@@ -1,1 +1,5 @@
+# If application.py has: app = Flask(__name__)
 from application import app
+
+# If application.py only has: application = Flask(__name__)
+from application import application as app
